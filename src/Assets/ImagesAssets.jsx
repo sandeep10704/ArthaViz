0ce649788025e246ba95ec7951f8ca0b4713a8b8
sandeep10704 <saivenkatasandeep3@gmail.com>
2025-07-04 17:26:24 +0530

    testing delopyment for that some changes happed in project in images assets

--- src/Assets/ImagesAssets.jsx
+++ src/Assets/ImagesAssets.jsx
@@ -1,48 +1,83 @@
-const ImagesAssets ={
-    poster1:require('./Images/product.png'),
-    Insta01:require('./Images/Insta01.jpg'),
-    Insta02:require('./Images/Insta02.jpg'),
-    Insta03:require('./Images/Insta03.jpg'),
-    Insta04:require('./Images/Insta04.jpg'),
-    Insta05:require('./Images/Insta05.jpg'),
-    Insta06:require('./Images/Insta06.jpg'),
-    Amazon:require('./Images/Amazon.png'),
-    FlipKart:require('./Images/FlipKart.png'),
-    Visa:require('./Images/visa.png'),
-    PayPal:require('./Images/PayPal.png'),
-    Post01:require('./Images/Post01.jpg'),
-    Post02:require('./Images/Post02.jpg'),
-    Post03:require('./Images/Post03.jpg'),
-    Post04:require('./Images/Post04.jpg'),
-    Categorie01:require('./Images/Categorie01.png'),
-    Categorie02:require('./Images/Categorie02.png'),
-    Categorie03:require('./Images/Categorie03.png'),
-    Categorie04:require('./Images/Categorie04.png'),
-    Categorie05:require('./Images/Categorie05.png'),
-    Categorie06:require('./Images/Categorie06.png'),
-    Selling01:require('./Images/Selling01.png'),
-    Selling02:require('./Images/Selling02.png'),
-    Selling03:require('./Images/Selling03.png'),
-    Selling04:require('./Images/Selling04.png'),
-    Selling05:require('./Images/Selling05.png'),
-    Item11:require('./Images/Item11.jpg'),
-    Item12:require('./Images/Item12.jpg'),
-    Item13:require('./Images/Item13.jpg'),
-    Item21:require('./Images/Item21.jpg'),
-    Item22:require('./Images/Item22.jpg'),
-    Item23:require('./Images/Item23.jpg'),
-    Item31:require('./Images/Item31.jpg'),
-    Item32:require('./Images/Item32.jpg'),
-    Item33:require('./Images/Item33.jpg'),
-    Item41:require('./Images/Item41.jpg'),
-    Item42:require('./Images/Item42.jpg'),
-    Item43:require('./Images/Item43.jpg'),
-    Poster02:require('./Images/Poster02.png'),
+import poster1 from './Images/product.png';
+import Insta01 from './Images/Insta01.jpg';
+import Insta02 from './Images/Insta02.jpg';
+import Insta03 from './Images/Insta03.jpg';
+import Insta04 from './Images/Insta04.jpg';
+import Insta05 from './Images/Insta05.jpg';
+import Insta06 from './Images/Insta06.jpg';
+import Amazon from './Images/Amazon.png';
+import FlipKart from './Images/FlipKart.png';
+import Visa from './Images/visa.png';
+import PayPal from './Images/PayPal.png';
+import Post01 from './Images/Post01.jpg';
+import Post02 from './Images/Post02.jpg';
+import Post03 from './Images/Post03.jpg';
+import Post04 from './Images/Post04.jpg';
+import Categorie01 from './Images/Categorie01.png';
+import Categorie02 from './Images/Categorie02.png';
+import Categorie03 from './Images/Categorie03.png';
+import Categorie04 from './Images/Categorie04.png';
+import Categorie05 from './Images/Categorie05.png';
+import Categorie06 from './Images/Categorie06.png';
+import Selling01 from './Images/Selling01.png';
+import Selling02 from './Images/Selling02.png';
+import Selling03 from './Images/Selling03.png';
+import Selling04 from './Images/Selling04.png';
+import Selling05 from './Images/Selling05.png';
+import Item11 from './Images/Item11.jpg';
+import Item12 from './Images/Item12.jpg';
+import Item13 from './Images/Item13.jpg';
+import Item21 from './Images/Item21.jpg';
+import Item22 from './Images/Item22.jpg';
+import Item23 from './Images/Item23.jpg';
+import Item31 from './Images/Item31.jpg';
+import Item32 from './Images/Item32.jpg';
+import Item33 from './Images/Item33.jpg';
+import Item41 from './Images/Item41.jpg';
+import Item42 from './Images/Item42.jpg';
+import Item43 from './Images/Item43.jpg';
+import Poster02 from './Images/Poster02.png';
 
+const ImagesAssets = {
+  poster1,
+  Insta01,
+  Insta02,
+  Insta03,
+  Insta04,
+  Insta05,
+  Insta06,
+  Amazon,
+  FlipKart,
+  Visa,
+  PayPal,
+  Post01,
+  Post02,
+  Post03,
+  Post04,
+  Categorie01,
+  Categorie02,
+  Categorie03,
+  Categorie04,
+  Categorie05,
+  Categorie06,
+  Selling01,
+  Selling02,
+  Selling03,
+  Selling04,
+  Selling05,
+  Item11,
+  Item12,
+  Item13,
+  Item21,
+  Item22,
+  Item23,
+  Item31,
+  Item32,
+  Item33,
+  Item41,
+  Item42,
+  Item43,
+  Poster02,
+};
 
-
-
-
-    
-}
 export default ImagesAssets;
